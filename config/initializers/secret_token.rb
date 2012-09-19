37929328efc1dc7c2ab9c35cface51fccac263a4
Ken Winter <kwinter@brighttalk.com>
2012-09-19 01:38:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7caed90230968034fdd2d8e84bd7cf90fde321313fd5d3fb9a591d30d2d5cee4d62ba911383c1dcebc2fe2780e5406dffb6b0dc4e64370fbd2101dd6bac07f73'
